698ddd013d5755be85fab66430c39059a551aa60
unknown <mahbuburovi1@gmail.com>
2025-09-15 12:23:45 -0700

    login css implement

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,8 @@
 <template>
   <h1 class="header">Login to Vue-Shop</h1>
   <div class="login-container">
-    <input type="email" v-model="email" placeholder="Email" class="input-field" />
-    <input type="password" v-model="password" placeholder="Password" class="input-field" />
+    <input type="email" v-model="email" placeholder="Email" class="input-field" /><br>
+    <input type="password" v-model="password" placeholder="Password" class="input-field" /><br>
     <button @click="onSumbit" class="login-btn">Submit</button>
   </div>
 </template>

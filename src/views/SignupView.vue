11ab6c7bd3e4431db0a81faa3125f3cc06df6725
unknown <jawadsadique2@gmail.com>
2025-09-20 15:57:36 +0600

    signup css added

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -11,8 +11,27 @@ const signup = () => {
 }
 </script>
 <template>
-  <input type="text" v-model="email" placeholder="E-Mail" />
-  <input type="password" v-model="password" placeholder="Password" />
-  <input type="text" v-model="username" placeholder="User Name" />
-  <button @click="signup">Signup</button>
+  <h1>Signup to Vue Shop</h1>
+  <div class="container">
+    <input type="text" v-model="email" placeholder="E-Mail" />
+    <input type="password" v-model="password" placeholder="Password" />
+    <input type="text" v-model="username" placeholder="User Name" />
+    <button class="hulk" @click="signup">Signup</button>
+  </div>
 </template>
+<style lang="scss" scoped>
+.container {
+  width: 200px;
+  margin: 50px auto;
+  padding: 7rem;
+  border-radius: 12px;
+  text-align: center;
+  background: #75d2e9;
+}
+.hulk {
+  background-color: #0c29cfa2;
+  color: aquamarine;
+  font-weight: bold;
+  border-radius: 08px;
+}
+</style>
